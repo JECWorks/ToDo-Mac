7cb42af0a2ed4a21bd4ffeaca225f9ff3efb0451
Jason Cox <jasoncox@mac.com>
2018-04-21 15:17:11 -0400

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Jason Cox on 4/21/18.
+//  Copyright © 2018 Jason Cox. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
